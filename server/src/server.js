9f3e3fd695687c5085acefe0b39b1a093e0b3bc9
Hossain Forhad <developerforhad24@gmail.com>
2026-03-25 10:34:15 +0600

    Middleware concept

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,38 @@
 const express = require("express");
-const morgan = require('morgan');
+const morgan = require("morgan");
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/test", (req,res) => {
-	res.status(200).send({
-		message: 'API is working fine!'
-	});
+const isLoggedIn = (req, res, next) => {
+  const login = true;
+
+  if (login) {
+
+    req.user = { id: 101 };
+    next();
+  } else {
+    res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+};
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "API is working fine!",
+  });
 });
 
+app.get("/api/user", isLoggedIn, (req, res) => {
+  console.log(req.user.id);
+  res.status(200).send({
+    message: "User profile retrieved successfully!",
+  });
+});
 
 app.listen(3001, () => {
-	console.log(`server is runnig on at http://localhost:3001`);
+  console.log(`server is runnig on at http://localhost:3001`);
 });
