24a91f2002bbd4c106993a1f37cef295c10fed1a
Hossain Forhad <developerforhad24@gmail.com>
2026-03-25 12:35:11 +0600

    HTTP errors

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const createError = require("http-errors");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/test", (req, res) => {
+  res.status(200).send({
+    message: "API is working fine!",
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  res.status(200).send({
+    message: "User profile retrieved successfully!",
+  });
+});
+
+// client side error handling
+app.use((req, res, next) => {
+  next(createError(404, "Route not found"));
+});
+
+// server side error handling -> all
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+  });
+});
+
+module.exports = app;
