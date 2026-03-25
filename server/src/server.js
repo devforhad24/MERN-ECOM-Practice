4f44146de5cbcd5f1ec760668eab06dbeb12ff32
Hossain Forhad <developerforhad24@gmail.com>
2026-03-25 12:16:13 +0600

    Error handling middlewire

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
 const app = express();
 
 app.use(morgan("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const isLoggedIn = (req, res, next) => {
   const login = true;
 
   if (login) {
-
     req.user = { id: 101 };
     next();
   } else {
@@ -26,11 +27,23 @@ app.get("/test", (req, res) => {
   });
 });
 
-app.get("/api/user", isLoggedIn, (req, res) => {
+app.get("/api/users", isLoggedIn, (req, res) => {
   console.log(req.user.id);
   res.status(200).send({
     message: "User profile retrieved successfully!",
   });
+});
+
+// client side error handling
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found!" });
+  next();
+});
+
+// server side error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(3001, () => {
