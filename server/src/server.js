74880f530f7def53a3af303acc0ae3cc2cecd24b
Hossain Forhad <developerforhad24@gmail.com>
2026-03-25 09:39:38 +0600

    HTTP req,res

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
-
 const app = express();
+
+app.get("/test", (req,res) => {
+	res.status(200).send({
+		message: 'API is working fine'
+	});
+});
+
 
 app.listen(3001, () => {
 	console.log(`server is runnig on at http://localhost:3001`);
