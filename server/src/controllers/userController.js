ab12049cc5f9d20f1cfce01f44e8b13efec1b030
Hossain Forhad <developerforhad24@gmail.com>
2026-03-26 16:50:24 +0600

    GET users form mongoDB and search, pagination setup

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,10 +1,43 @@
 const createError = require("http-errors");
+const User = require("../models/userModel");
 
 // fetch users
-const getUsers = (req, res, next) => {
+const getUsers = async (req, res, next) => {
   try {
+    const search = req.query.search || "";
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 2;
+
+    const searchRegEXP = new RegExp(".*" + search + ".*", "i");
+
+    const filter = {
+      isAdmin: { $ne: true },
+      $or: [
+        { name: { $regex: searchRegEXP } },
+        { email: { $regex: searchRegEXP } },
+        { phone: { $regex: searchRegEXP } },
+      ],
+    };
+
+    const options = { password: 0 };
+
+    const users = await User.find(filter, options)
+      .limit(limit)
+      .skip((page - 1) * limit);
+
+    const count = await User.find(filter).countDocuments();
+
+    if(!users) throw createError(404, 'No users found');
+
     res.status(200).send({
-      message: "Users retrieved successfully!"
+      message: "Users retrieved successfully!",
+      users,
+      pagination:{
+        totalPages: Math.ceil(count / limit),
+        currentPage: page,
+        previousPage: page - 1 > 0 ? page - 1 : null,
+        nextPage: page + 1 <= Math.ceil(count / limit) ? page + 1 : null,
+      },
     });
   } catch (error) {
     next(error);
